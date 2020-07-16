a4742910c5a19a60c375a53bb3f5d61b3707cb46
Nestor <ncervan87@gmail.com>
2020-07-16 13:36:29 -0700

    use style-components on nav and main

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,33 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    text-align: center;
+    margin: 2% auto;
+    width: 65%;
+`
+
+const Paragraph = styled.p`
+    color: #0b0c0c;
+    font-family: Quicksand;
+    width: 90%;
+    margin: 1% auto;
+    line-height: 1.3;
+    background: #8899a2;
+    padding: 1%;
+    border-radius: 5px;
+`
+
+const Button = styled.button`
+    background: transparent;
+    color: #fff;
+    border: 2px solid #fff;
+    font-size: 0.9rem;
+    padding: 1% 2%;
+    border-radius: 7px;
+    font-family: Quicksand;
+    margin-top: 1%;
+`
 
 function Main(props) {
     const { current, explanation } = props
@@ -12,25 +41,24 @@ function Main(props) {
 
     if (!props.current) {
         return (
-            <div className="container">
+            <Container>
                 <div className="placeholderImg">
-
                 </div>
-                <p className="placeholderTxt"></p>
+                <Paragraph></Paragraph>
                 <button onClick={() => {
                     setCount(count + 1)
                     console.log(count)
                 }}>Re-render</button>
-            </div>
+            </Container>
         )
     }
 
     return (
-        <div className="container">
+        <Container>
             <img className="img-main" src={current} alt="main nasa" />
-            <p className="p-main"> {explanation} </p>
-            <button onClick={() => setCount(count + 1)}>Re-render</button>{console.log("rendered app")}
-        </div>
+            <Paragraph> {explanation} </Paragraph>
+            <Button onClick={() => setCount(count + 1)}>Re-render</Button>{console.log("rendered app")}
+        </Container>
     )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,42 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Navi = styled.nav`
+    font-size: 1.25rem;
+    font-family: 'Karla';
+    display: flex;
+    justify-content: flex-end;
+`
+
+const Ulist = styled.ul`
+    list-style: none;
+    display: flex;
+    padding: 1%;
+`
+
+const Header = styled.header`
+    background: #505c63;
+    padding: 1rem;
+`
+
+const Item = styled.li`
+    margin-right: 4%;
+    &:hover {
+        cursor: pointer;
+    }
+`
 
 function Nav() {
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+        <Header>
+            <Navi>
+                <Ulist>
+                    <Item>Home</Item>
+                    <Item>About</Item>
+                    <Item>Contact</Item>
+                </Ulist>
+            </Navi>
+        </Header>
     )
 }
 
